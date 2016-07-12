305f9f1a4e3b07a4762a3b9bc28832d1c49b8b23
Chris Carty <chriscartydev@gmail.com>
2016-07-12 15:34:42 -0400

    added leadlet to static

--- geocoder/geocoder/settings.py
+++ geocoder/geocoder/settings.py
@@ -129,11 +129,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(BASE_DIR, 'static'),
