b1dfc8857cac8dae9be87361b181338121f38792
Chris Carty <chriscartydev@gmail.com>
2018-01-07 21:04:22 -0500

    removed docker data

--- geocoder/settings.py
+++ geocoder/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET")
+SECRET_KEY = os.environ.get("secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
